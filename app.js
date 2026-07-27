2f7a54793db92a072ff04c911e7c304ff67422f6
Theo3535 <105073887+Theo3535@users.noreply.github.com>
2026-07-27 05:33:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "https://backend-six-liart-25.vercel.app";
+const API = "https://backend-six-liart-28.vercel.app";
 
 const connectBtn = document.getElementById("connectBtn");
 const transferBtn = document.getElementById("transferBtn");
